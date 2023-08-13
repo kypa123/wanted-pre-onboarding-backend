5dfc959c51c9414a0596874076fba682109adaa5
Jiho Joo <aksjajsxm123@gmail.com>
2023-08-13 13:54:22 +0900

    [추가]userController 추가

---
+++ src/controller/user-controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import UserService from '../services/user-service.ts';
+import { TokenRequest } from '../interfaces/jwt-token-validation.ts';
+
+export default class UserController {
+    private userService: UserService;
+    constructor(UserService: UserService) {
+        this.userService = UserService;
+    }
+
+    async addUser(req: Request, res: Response) {
+        const userData = req.body;
+        const result = await this.userService.addUser(userData);
+        if (result.message == 'validationError') {
+            res.status(401).json(result);
+        } else {
+            res.json(result);
+        }
+    }
+    async deleteUser(req: TokenRequest, res: Response) {
+        const userInfo = req.tokenInfo;
+        const result = await this.userService.deleteUser(userInfo.id);
+        res.clearCookie('token').json(result);
+    }
+
+    async login(req: Request, res: Response) {
+        const userInfo = req.body;
+        const result = await this.userService.login(userInfo);
+        if (result.message == 'validationError') {
+            res.status(401).json(result);
+        } else {
+            res.cookie('token', result.body).status(200).json(result);
+        }
+    }
+}
