0383a3a33f3da82d2ae2e2c621a44f15ebf942d8
Jiho Joo <aksjajsxm123@gmail.com>
2023-08-14 13:53:54 +0900

    [추가]createRouterInstance, RouterInstance export

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,21 @@
 import PostRouter from './post-router.ts';
 import UserRouter from './user-router.ts';
+import { UserController, PostController } from '../controller/index.ts';
+import { UserService, PostService } from '../services/index.ts';
+import { User, Post, Credential, PostContent, sequelize } from '../db/index.ts';
+import { createRouterInstance } from '../utils/useful_functions.ts';
+const UserRouterInstance = createRouterInstance(
+    UserRouter,
+    UserController,
+    UserService,
+    [User, Credential, sequelize],
+);
 
-export { PostRouter, UserRouter };
+const PostRouterInstance = createRouterInstance(
+    PostRouter,
+    PostController,
+    PostService,
+    [Post, PostContent, sequelize],
+);
+
+export { PostRouter, UserRouter, PostRouterInstance, UserRouterInstance };
