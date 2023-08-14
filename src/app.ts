c3de51f42512a237161b3ebdab7ed09981b4e60d
Jiho Joo <aksjajsxm123@gmail.com>
2023-08-14 15:33:19 +0900

    [추가]userRouter, PostRouter express.app에 등록

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { errorHandler } from './middlewares/index.ts';
-import { viewsRouter } from './routes/index.ts';
+import { UserRouterInstance, PostRouterInstance } from './routes/index.ts';
 
 const app = express();
 
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(viewsRouter);
-
+app.use('/users', UserRouterInstance.getRouter());
+app.use('/posts', PostRouterInstance.getRouter());
 //존재하지 않는 라우팅이라면 404, 이외의 에러는 errorHandler로
 app.use(function (req, res, next) {
     next(createError(404));
