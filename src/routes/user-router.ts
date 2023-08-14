e37ee51e247b101061a731a050cd61f8c6e2c6bc
Jiho Joo <aksjajsxm123@gmail.com>
2023-08-14 13:03:50 +0900

    [추가]userRouter 추가

---
+++ src/routes/user-router.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { isLoggedIn, asyncHandler } from '../middlewares/index.ts';
+import { UserController } from '../controller/index.ts';
+export default class UserRouter {
+    private router: Router;
+    private userController: UserController;
+    constructor(userController: UserController) {
+        this.router = Router();
+        this.userController = userController;
+
+        this.router.post('/login', asyncHandler(this.userController.login));
+        this.router.post(
+            '/logout',
+            isLoggedIn,
+            asyncHandler(this.userController.logout),
+        );
+        this.router.post('/', asyncHandler(this.userController.addUser));
+        this.router.delete(
+            '/',
+            isLoggedIn,
+            asyncHandler(this.userController.deleteUser),
+        );
+    }
+}
