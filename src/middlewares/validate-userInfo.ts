3b4016c3ecde8c036e92091e411992fafcded577
Jiho Joo <aksjajsxm123@gmail.com>
2023-08-15 16:06:03 +0900

    [추가]validation-userInfo middleware 추가

---
+++ src/middlewares/validate-userInfo.ts
@@ -0,0 +1,24 @@
+import { validatePassword, validateEmail } from '../utils/useful_functions.ts';
+import { Request, Response, NextFunction } from 'express';
+import { TokenRequest } from '../interfaces/jwt-token-validation.ts';
+async function validateUserinfo(
+    req: Request | TokenRequest,
+    res: Response,
+    next: NextFunction,
+) {
+    const userInfo = req.body;
+    const validatePasswordResult = validatePassword(userInfo.password);
+    const validateEmailResult = validateEmail(userInfo.email);
+    if (validatePasswordResult && validateEmailResult) {
+        next();
+    } else {
+        if (!validateEmailResult) {
+            res.status(401).json({ message: 'email validation error' });
+        }
+        if (!validatePasswordResult) {
+            res.status(401).json({ message: 'password validation error' });
+        }
+    }
+}
+
+export default validateUserinfo;
