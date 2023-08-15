c954acb2bc4a9a1c732fa1853dc803d50a68cc94
Jiho Joo <aksjajsxm123@gmail.com>
2023-08-15 18:18:27 +0900

    [수정]의미없는 env참조 삭제

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,5 @@ export default {
     dbConn: process.env.DB_CONNECTION,
     jwtSecret: process.env.JWT_SECRET_KEY,
     jwtExpire: process.env.JWT_EXPIRE_DATE,
-    dataAPI: process.env.DATA_API,
     bcryptSalt: process.env.SALT,
 };
